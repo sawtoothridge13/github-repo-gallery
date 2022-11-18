19b2c9fde90c0a7d8604d24f93b172189418cde3
sawtoothridge13 <sawtoothridge13@gmail.com>
2022-11-18 17:17:59 +0100

    Added dynamic search

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const username = "sawtoothridge13";
 const repoList = document.querySelector(".repo-list");
 const allReposContainer = document.querySelector(".repos");
 const repoData = document.querySelector(".repo-data");
+const viewReposButton = document.querySelector(".view-repos");
+const filterInput = document.querySelector(".filter-repos");
+
 
 const gitUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -40,6 +43,7 @@ const gitRepos = async function () {
 
 
 const displayRepos = function (repos) {
+    filterInput.classList.remove("hide");
     for (const repo of repos) {
         const repoItem = document.createElement("li");
         repoItem.classList.add("repo");
@@ -76,6 +80,7 @@ const displayRepoInfo = function (repoInfo, languages) {
     repoData.innerHTML = "";
     repoData.classList.remove("hide")
     allReposContainer.classList.add("hide");
+    viewReposButton.classList.remove("hide");
     const div = document.createElement("div");
 
     div.innerHTML = `
@@ -88,3 +93,23 @@ const displayRepoInfo = function (repoInfo, languages) {
     repoData.append(div);
 };
 
+viewReposButton.addEventListener("click", function () {
+    allReposContainer.classList.remove("hide");
+    repoData.classList.add("hide");
+    viewReposButton.classList.add("hide");
+});
+
+filterInput.addEventListener("input", function (e) {
+    const searchText = e.target.value;
+    const repos = document.querySelectorAll(".repo");
+    const searchLowerText = searchText.toLowerCase();
+
+    for (const repo of repos) {
+        const repoLowerText = repo.innerText.toLowerCase();
+        if (repoLowerText.includes(searchLowerText)) {
+            repo.classList.remove("hide");
+        } else {
+            repo.classList.add("hide");
+        }
+    }
+});
